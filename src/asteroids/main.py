bf0fc6e96badec17402bd6460d32fee77da4dd95
malathair <malathair@spectregate.net>
2024-08-20 21:20:10 -0600

    Added player, asteroids, and collision detection

--- src/asteroids/main.py
+++ src/asteroids/main.py
@@ -2,14 +2,28 @@ import pygame
 
 import constants
 
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    asteroids = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = updatable
+    Player.containers = (drawable, updatable)
+
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+    field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -17,7 +31,20 @@ def main():
                 return
 
         screen.fill(pygame.Color(0, 0, 0, 255))
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        for obj in asteroids:
+            if player.collision_check(obj):
+                print("Game over!")
+                return
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
